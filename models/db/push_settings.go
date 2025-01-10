25c5a28b94f2b0387b40daa990fb7177e79aa08d
nPervushin <pervushin@mnfctr.ru>
2025-01-10 15:13:43 +0400

    Настройка пушей

--- models/db/push_settings.go
+++ models/db/push_settings.go
@@ -7,7 +7,7 @@ import (
 
 type SpacePushSetting struct {
 	BaseSpaceModel
-	SpaceUserID string                      `gorm:"type:varchar(36);uniqueIndex:idx_user"`
+	SpaceUserID string                      `gorm:"type:varchar(36);index:idx_user"`
 	Code        models.SpacePushSettingCode `gorm:"type:varchar(255);index:idx_setting_code"`
 	SystemValue *bool
 	EmailValue  *bool
