f53c8195f29c6bbd66e20538a2e6422f897e4438
nPervushin <pervushin@mnfctr.ru>
2025-03-25 14:33:26 +0400

    кэш проверки подключения

--- lib/external-services/worker/status_upd_worker.go
+++ lib/external-services/worker/status_upd_worker.go
@@ -72,15 +72,22 @@ func (i impl) handle(ctx context.Context, integrationName string, jobHandler Sta
 		logger.WithError(err).Error("ошибка получения списка активных спейсов")
 		return
 	}
+	connectedMap := make(map[string]bool, len(ids))
 	for _, spaceID := range ids {
 		if helpers.IsContextDone(ctx) {
 			return
 		}
-		logger = logger.WithField("space_id", spaceID)
-		if !jobHandler.CheckConnected(ctx, spaceID) {
+		isConnected, ok := connectedMap[spaceID]
+		if !ok {
+			isConnected = jobHandler.CheckConnected(ctx, spaceID)
+			connectedMap[spaceID] = isConnected
+		}
+		if !isConnected {
 			continue
 		}
 
+		logger = logger.WithField("space_id", spaceID)
+		
 		list, err := jobHandler.GetCheckList(ctx, spaceID, models.VacancyPubStatusModeration)
 		if err != nil {
 			logger.
